1548c1f064277ef7405fedf9f816325f4f28db81
FacuRRemiro <129411989+FacuRRemiro@users.noreply.github.com>
2023-03-30 12:19:10 -0300

    Add files via upload

---
+++ Sistema de control/src/Admin.java
@@ -0,0 +1,356 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Facu
+ */
+public class Admin extends javax.swing.JFrame {
+
+    /**
+     * Creates new form Admin
+     */
+    public Admin() {
+        initComponents();
+        
+    }
+
+    void cargagri1(String valor){
+        String[]titulos={"NOMBRE","APELLIDO","FECHA","HORA"};
+        String[]registro=new String[4];
+        DefaultTableModel model = new DefaultTableModel(null,titulos);
+        String sql="SELECT*FROM entrada WHERE nombre LIKE '%"+valor+"%'";
+        
+        try { 
+            String db="net.ucanaccess.jdbc.UcanaccessDriver";
+            Connection cn;
+            cn = DriverManager.getConnection("jdbc:ucanaccess://E://Programacion//PRUEBA//TP JAVA//registro.accdb");
+            Statement st = cn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+        
+            while(rs.next()){
+                registro[0]=rs.getString("nombre");
+                registro[1]=rs.getString("apellido");
+                registro[2]=rs.getString("fecha");
+                registro[3]=rs.getString("hora");
+                model.addRow(registro);
+            }
+            
+            jTable1.setModel(model);
+        }catch(Exception ex){
+            JOptionPane.showMessageDialog(null, ex);
+        }
+  
+    }
+    
+     void cargagri11(String valor){
+        String[]titulos={"NOMBRE","APELLIDO","FECHA","HORA"};
+        String[]registro=new String[4];
+        DefaultTableModel model = new DefaultTableModel(null,titulos);
+        String sql="SELECT*FROM retiro WHERE nombre LIKE '%"+valor+"%'";
+        
+        try { 
+            String db="net.ucanaccess.jdbc.UcanaccessDriver";
+            Connection cn;
+            cn = DriverManager.getConnection("jdbc:ucanaccess://E://Programacion//PRUEBA//TP JAVA//registro.accdb");
+            Statement st = cn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+        
+            while(rs.next()){
+                registro[0]=rs.getString("nombre");
+                registro[1]=rs.getString("apellido");
+                registro[2]=rs.getString("fecha");
+                registro[3]=rs.getString("hora");
+                model.addRow(registro);
+            }
+            
+            jTable2.setModel(model);
+        }catch(Exception ex){
+            JOptionPane.showMessageDialog(null, ex);
+        }
+  
+    }
+    
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jLabel4 = new javax.swing.JLabel();
+        buscarcliente = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jScrollPane4 = new javax.swing.JScrollPane();
+        jTable2 = new javax.swing.JTable();
+        exit1 = new javax.swing.JButton();
+        atras = new javax.swing.JButton();
+        dia = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(33, 33, 32));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel4.setFont(new java.awt.Font("MS Reference Sans Serif", 1, 14)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setText("RETIRO");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 240, -1, -1));
+
+        buscarcliente.setBackground(new java.awt.Color(41, 46, 73));
+        buscarcliente.setForeground(new java.awt.Color(255, 255, 255));
+        buscarcliente.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        buscarcliente.setBorder(null);
+        buscarcliente.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buscarclienteActionPerformed(evt);
+            }
+        });
+        buscarcliente.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                buscarclienteKeyReleased(evt);
+            }
+        });
+        jPanel1.add(buscarcliente, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 20, 120, 30));
+
+        jButton1.setBackground(new java.awt.Color(72, 85, 99));
+        jButton1.setFont(new java.awt.Font("MS Reference Sans Serif", 1, 12)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(255, 255, 255));
+        jButton1.setText("Mostrar todos");
+        jButton1.setBorder(null);
+        jButton1.setBorderPainted(false);
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 20, 140, 30));
+
+        jTable1.setBackground(new java.awt.Color(187, 210, 197));
+        jTable1.setFont(new java.awt.Font("MS Reference Sans Serif", 0, 12)); // NOI18N
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "NOMBRE", "APELLIDO", "FECHA", "HORA"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jTable1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jTable1MouseClicked(evt);
+            }
+        });
+        jScrollPane3.setViewportView(jTable1);
+
+        jPanel1.add(jScrollPane3, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 140, -1, 80));
+
+        jLabel5.setFont(new java.awt.Font("MS Reference Sans Serif", 1, 14)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel5.setText("NOMBRE DEL PROFESOR");
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 30, -1, -1));
+
+        jLabel6.setFont(new java.awt.Font("MS Reference Sans Serif", 1, 14)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel6.setText("INGRESO");
+        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 110, -1, -1));
+
+        jTable2.setBackground(new java.awt.Color(187, 210, 197));
+        jTable2.setFont(new java.awt.Font("MS Reference Sans Serif", 0, 12)); // NOI18N
+        jTable2.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "NOMBRE", "APELLIDO", "FECHA", "HORA"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jTable2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jTable2MouseClicked(evt);
+            }
+        });
+        jScrollPane4.setViewportView(jTable2);
+
+        jPanel1.add(jScrollPane4, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 270, -1, 80));
+
+        exit1.setBackground(new java.awt.Color(72, 85, 99));
+        exit1.setFont(new java.awt.Font("MS Reference Sans Serif", 1, 12)); // NOI18N
+        exit1.setForeground(new java.awt.Color(255, 255, 255));
+        exit1.setText("SALIR");
+        exit1.setBorderPainted(false);
+        exit1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                exit1ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(exit1, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 450, 80, 20));
+
+        atras.setBackground(new java.awt.Color(72, 85, 99));
+        atras.setFont(new java.awt.Font("MS Reference Sans Serif", 1, 12)); // NOI18N
+        atras.setForeground(new java.awt.Color(255, 255, 255));
+        atras.setText("ATRAS");
+        atras.setBorderPainted(false);
+        atras.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                atrasActionPerformed(evt);
+            }
+        });
+        jPanel1.add(atras, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 450, 100, 20));
+
+        dia.setBackground(new java.awt.Color(72, 85, 99));
+        dia.setFont(new java.awt.Font("MS Reference Sans Serif", 1, 12)); // NOI18N
+        dia.setForeground(new java.awt.Color(255, 255, 255));
+        dia.setText("BUSCAR P/ DIA");
+        dia.setBorderPainted(false);
+        dia.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                diaActionPerformed(evt);
+            }
+        });
+        jPanel1.add(dia, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 450, 150, 20));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void buscarclienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buscarclienteActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_buscarclienteActionPerformed
+
+    private void buscarclienteKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_buscarclienteKeyReleased
+        // TODO add your handling code here:
+        cargagri1(buscarcliente.getText());
+        cargagri11(buscarcliente.getText());
+    }//GEN-LAST:event_buscarclienteKeyReleased
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        cargagri1("");
+        
+        buscarcliente.setText("");
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jTable1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTable1MouseClicked
+        
+    }//GEN-LAST:event_jTable1MouseClicked
+
+    private void jTable2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTable2MouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTable2MouseClicked
+
+    private void exit1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_exit1ActionPerformed
+
+        dispose();
+    }//GEN-LAST:event_exit1ActionPerformed
+
+    private void atrasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_atrasActionPerformed
+
+        Consultar1 newframe = new Consultar1();
+        newframe.setVisible(true);
+        this.dispose();
+
+    }//GEN-LAST:event_atrasActionPerformed
+
+    private void diaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_diaActionPerformed
+        Admin_dia newframe = new Admin_dia();
+        newframe.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_diaActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Admin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Admin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Admin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Admin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Admin().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton atras;
+    private javax.swing.JTextField buscarcliente;
+    private javax.swing.JButton dia;
+    private javax.swing.JButton exit1;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JScrollPane jScrollPane4;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTable jTable2;
+    // End of variables declaration//GEN-END:variables
+}
